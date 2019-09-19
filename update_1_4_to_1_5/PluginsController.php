4c3f1a917d279ebcccc2a4de8834a53ca397e531
Cherry-toto <2581047041@qq.com>
2019-09-20 07:49:19 +0800

    新增v1.5升级插件

---
+++ update_1_4_to_1_5/PluginsController.php
@@ -0,0 +1,146 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | JiZhiCMS { 极致CMS，给您极致的建站体验 }  
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018-2099 http://www.jizhicms.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 留恋风 <2581047041@qq.com>
+// +----------------------------------------------------------------------
+// | Date：2019/09/15
+// +----------------------------------------------------------------------
+
+namespace A\exts;
+
+use FrPHP\lib\Controller;
+use FrPHP\Extend\Page;
+class PluginsController extends Controller {
+	
+	
+	//自动执行
+	public function _init(){
+		/**
+			继承系统默认配置
+		
+		**/
+		
+		//检查当前账户是否合乎操作
+		if(!isset($_SESSION['admin']) || $_SESSION['admin']['id']==0){
+			Redirect(U('Login/index'));
+			
+		}
+ 
+	    if($_SESSION['admin']['isadmin']!=1){
+		    if(strpos($_SESSION['admin']['paction'],','.APP_CONTROLLER.',')!==false){
+		   
+		    }else{
+			     $action = APP_CONTROLLER.'/'.APP_ACTION;
+			if(strpos($_SESSION['admin']['paction'],','.$action.',')==false){
+				$ac = M('Ruler')->find(array('fc'=>$action));
+			   Error('您没有【'.$ac['name'].'】的权限！',U('Index/index'));
+			}
+		}
+	   
+	  
+	  }
+	  
+	    $webconf = webConf();
+	    $this->webconf = $webconf;
+	    $customconf = get_custom();
+	    $this->customconf = $customconf;
+		
+		//插件模板页目录
+		
+		$this->tpl = '@'.dirname(__FILE__).'/tpl/';
+		
+		/**
+			在下面添加自定义操作
+		**/
+		
+		
+	}
+	
+	//执行SQL语句在此处处理,或者移动文件也可以在此处理
+	public  function install(){
+		//检测版本号
+		if($this->webconf['web_version']!='1.4'){
+			JsonReturn(['code'=>1,'msg'=>'您的软件系统版本为'.$this->webconf['web_version'].'，该插件仅支持1.4版本升级到1.5版本！']);
+		}
+		//更新系统版本
+		M('sysconfig')->update(['field'=>'web_version'],['data'=>'1.5']);
+		
+		//新增公共配置
+		if(!M('sysconfig')->find(['field'=>'domain'])){
+			M('sysconfig')->add(['field'=>'domain','title'=>'网站网址','tip'=>'全局网址,最后不带/(斜杠)','type'=>0,'data'=>'']);
+		}
+		setCache('webconfig',null);
+		//更换文件
+		$filelist = [
+			APP_PATH.'A/exts/update_1_4_to_1_5/file/fields-add.html'=>APP_PATH.'A/t/tpl/fields-add.html',
+			APP_PATH.'A/exts/update_1_4_to_1_5/file/fields-edit.html'=>APP_PATH.'A/t/tpl/fields-edit.html',
+			APP_PATH.'A/exts/update_1_4_to_1_5/file/member-add.html'=>APP_PATH.'A/t/tpl/member-add.html',
+			APP_PATH.'A/exts/update_1_4_to_1_5/file/member-edit.html'=>APP_PATH.'A/t/tpl/member-edit.html',
+			APP_PATH.'A/exts/update_1_4_to_1_5/file/sys.html'=>APP_PATH.'A/t/tpl/sys.html',
+			APP_PATH.'A/exts/update_1_4_to_1_5/file/Functions.php'=>APP_PATH.'Conf/Functions.php',
+			APP_PATH.'A/exts/update_1_4_to_1_5/file/Page.php'=>APP_PATH.'FrPHP/Extend/Page.php',
+			APP_PATH.'A/exts/update_1_4_to_1_5/file/Fr.php'=>APP_PATH.'FrPHP/Fr.php',
+			APP_PATH.'A/exts/update_1_4_to_1_5/file/CommonController.php'=>APP_PATH.'Home/c/CommonController.php',
+			APP_PATH.'A/exts/update_1_4_to_1_5/file/HomeController.php'=>APP_PATH.'Home/c/HomeController.php',
+			APP_PATH.'A/exts/update_1_4_to_1_5/file/LoginController.php'=>APP_PATH.'Home/c/LoginController.php',
+			APP_PATH.'A/exts/update_1_4_to_1_5/file/UserController.php'=>APP_PATH.'Home/c/UserController.php',
+			APP_PATH.'A/exts/update_1_4_to_1_5/file/README.md'=>APP_PATH.'FrPHP/lib/README.md',
+			APP_PATH.'A/exts/update_1_4_to_1_5/file/readme.txt'=>APP_PATH.'readme.txt',
+			APP_PATH.'A/exts/update_1_4_to_1_5/file/View.php'=>APP_PATH.'FrPHP/lib/View.php',
+			APP_PATH.'A/exts/update_1_4_to_1_5/file/FieldsController.php'=>APP_PATH.'A/c/FieldsController.php',
+			
+		];
+		
+		foreach($filelist as $k=>$v){
+			copy($k,$v);
+		}
+		
+	
+		
+		
+		
+		return true;
+		
+	}
+	
+	
+	//卸载程序,对新增字段、表等进行删除SQL操作，或者其他操作
+	public function uninstall(){
+		//下面是删除test表的SQL操作
+		
+		return true;
+	}
+	
+	//安装页面介绍,操作说明
+	public function desc(){
+		
+		$this->display($this->tpl.'plugins-description.html');
+	}
+	
+	//配置文件,插件相关账号密码等操作
+	public  function setconf($plugins){
+		//将插件赋值到模板中
+		$this->plugins = $plugins;
+		$this->config = json_decode($plugins['config'],1);
+		
+		$this->display($this->tpl.'plugins-body.html');
+	}
+	//获取插件内提交的数据处理
+	public function setconfigdata($data){
+		
+		M('plugins')->update(['id'=>$data['id']],['config'=>json_encode($data,JSON_UNESCAPED_UNICODE)]);
+		setCache('hook',null);//清空hook缓存
+		JsonReturn(['code'=>0,'msg'=>'设置成功！']);
+		
+	}
+	
+	
+}
+
+
+
+
---
+++ update_1_4_to_1_5/file/README.md
@@ -0,0 +1,46 @@
+# 极致CMS建站系统
+## 一款免费开源的PHP建站CMS系统，可以免授权商业使用该系统。
+
+[![极致CMS建站系统](https://img.kancloud.cn/78/9f/789f15c672f807686ab8b9edab6f35f1_1920x700.png)](http://down.jizhicms.cn/jizhicms_Beta1.4.zip)
+
+官网：[jizhicms.cn](http://jizhicms.cn) |&nbsp;&nbsp;&nbsp;[jizhicms.com](http://jizhicms.cn)   
+演示：[demo.jizhicms.cn](http://demo.jizhicms.cn)   后台：[http://demo.jizhicms.cn/admin.php](http://demo.jizhicms.cn/admin.php) [账户：test,密码：123456]  
+作者：留恋风（QQ：2581047041,微信：TF-2581047041）  
+版权：廊坊市极致网络科技有限公司    
+公司联系电话：0316-2222616     
+商务合作：13785477600  13660240863    
+QQ交流群：774764507（官方群）[【加群】](https://jq.qq.com/?_wv=1027&k=56VstDU)     
+*****
+极致CMS（以下简称:JIZHICMS）是一款免费开源的PHP建站CMS系统，在同意条款下可以免授权商业使用该系统。
+> 1. jizhicms的开发语言是PHP，支持PHP5.6+，发布之日起就已经兼容到PHP7.3版本，推荐使用PHP7.0版本以上。
+> 2. 安装环境建议Linux服务器，当然Windows也是可以安装的，云服务器建议安装宝塔面板【[查看](http://www.bt.cn/)】
+> 3. 无论是Windows还是Linux建议安装Apache、mysql、phpMyAdmin配件
+> 4. 本系统当前版本(v1.x)仅支持mysql数据库
+
+*****
+## 安装须知  
+* PHP5.6+   
+* 数据库目前仅支持MySQL，版本5.0+，数据编码utf-8或者utf8-general-ci   
+* 部署环境需支持伪静态    
+
+## 开始安装  
+1. 下载源码并解压源码到空间根目录(www)      
+2. 浏览器访问http://localhost/install/ (本地环境) http://绑定域名/install/ (空间服务器环境)   
+3. 进入安装步骤，逐步进行安装   
+4. 初次安装请安装备份或者测试数据    
+5. 安装完毕后，为了安全，请删除根目录intall安装文件夹    
+
+## 网站搬家   
+1. 进入后台--【系统扩展】--【数据库列表】，点击 <kbd>备份数据库</kbd> 将当前数据库备份    
+2. 将cache文件夹下面的缓存文件全部删除，并在根目录放置官方源码里的install文件夹        
+3. 将整个根目录打包下载到本地    
+4. 将打包后的压缩包上传到新空间   
+5. 按照【开始安装】的步骤进行安装程序     
+6. 安装第二步填入数据库账号密码后，选择你最新备份的数据库，点击进行安装    
+7. 安装完毕，为了安全，请删除根目录intall安装文件夹   
+
+## 许可证  
+MIT    
+
+![极致CMS建站系统](https://img.kancloud.cn/2f/32/2f3270ea08ba9675a2105beebb503652_1920x273.png)
+
